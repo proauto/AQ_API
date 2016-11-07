c5d5e89d8e9f5b7e7ea6934e0d77d7672155b16c
proauto <hg1771@naver.com>
2016-11-08 02:26:44 +0900

    add facebook webhooker

--- mysite/quickstart/views.py
+++ mysite/quickstart/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+from django.http import HttpResponse
+from rest_framework import viewsets, status
 from quickstart.serializers import UserSerializer, GroupSerializer
 
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -11,3 +15,10 @@ class UserViewSet(viewsets.ModelViewSet):
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+@api_view(['GET'])
+def FaceBookView(request):
+        if request.GET['hub.verify_token'] == 'WebHook':
+            return Response(request.GET['hub.challenge'])
+        else:
+            return Response('Error, invalid token')
