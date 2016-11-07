09e636e3f120164bb725d48bde572a26e2dd3683
proauto <hg1771@naver.com>
2016-11-08 00:06:13 +0900

    add test bookmarkapp

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.contrib import admin
 from rest_framework import routers
-from mysite.quickstart import views
+from quickstart import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -24,6 +25,7 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
